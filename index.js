39ecbf72fe94e017a8fe890511934d42eac43f6e
Manzi <phmutman@hotmail.com>
2019-02-02 23:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(kittens){
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(kittens){
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(kittens){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(kittens){
+  return kittens.shift()
+}
+
+function removeLastKitten(kittens){
+  return kittens.slice(0, (kittens.length - 1))
+}
+
+function appendKitten(kitten){
+  return kittens.concat(kitten)
+}
+
+function prependKitten(kitten){
+  return kittens.concat(kitten)
+}
+
+function removeFirstKitten(kittens){
+  return (kittens.slice(1))
+}
--- test/index-test.js
+++ test/index-test.js
@@ -58,19 +58,19 @@ describe('Arrays', function() {
     })
   })
 
-  describe('removeLastKitten()', function() {
+  describe('removeLastKitten(kittens)', function() {
     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
+      expect(removeLastKitten(kittens)).toEqual(["Milo", "Otis"])
 
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
     })
   })
 
-  describe('removeFirstKitten()', function() {
+  describe('removeFirstKitten(kittens)', function() {
     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
+      expect(removeFirstKitten(kittens)).toEqual(["Otis", "Garfield"])
 
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
     })
